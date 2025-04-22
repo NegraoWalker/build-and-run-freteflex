557c07a13b9de76073d0f7c959a887f60ed60dc4
Walker Negrao <1882452@tcs.com>
2025-04-22 10:48:47 -0300

    Atualização

--- src/main/java/com/walker/build_and_run_freteflex/controller/ShippingController.java
+++ src/main/java/com/walker/build_and_run_freteflex/controller/ShippingController.java
@@ -1,9 +1,22 @@
 package com.walker.build_and_run_freteflex.controller;
 
+import com.walker.build_and_run_freteflex.controller.dto.ShippingResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class ShippingController {
+
+    @GetMapping(value = "/shipping/calculate")
+    public ResponseEntity<ShippingResponse> calculate(@RequestParam("type") String shippingType,
+                                                      @RequestParam("distance") Double distance,
+                                                      @RequestParam("weight") Double weight) {
+
+        //TODO - CHAMAR SERVICE QUE IMPLEMENTA O CÁLCULO DO FRETE
+        return ResponseEntity.ok(new ShippingResponse(0.0));
+    }
 }
---
+++ src/main/java/com/walker/build_and_run_freteflex/controller/dto/ShippingResponse.java
@@ -0,0 +1,4 @@
+package com.walker.build_and_run_freteflex.controller.dto;
+
+public record ShippingResponse(Double shippingCost) {
+}
